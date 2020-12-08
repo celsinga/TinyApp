39daa6d1ec3e7357b9f8c58ddece8f12881a8ba0
Colin <colinelsinga@gmail.com>
2020-12-08 06:40:02 +0000

    new function

--- express_server.js
+++ express_server.js
@@ -11,6 +11,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  var alphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < alphaNum.length; i++) {
+    if (result.length < 6) {
+      result += alphaNum.charAt(Math.floor(Math.random() * alphaNum.length));
+    }
+  }
+  return result;
+}
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
