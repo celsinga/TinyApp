5344f463edb9b527d822cb1930936a3807f12c7e
Colin <colinelsinga@gmail.com>
2020-12-09 02:15:49 +0000

    added edit/update url feature

--- express_server.js
+++ express_server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
+///
+///
+// Database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Generate short URL
 function generateRandomString() {
   var alphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
@@ -22,25 +24,35 @@ function generateRandomString() {
   return result;
 };
 
+//Create URL
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
-
-app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect(`/urls`);
-});
-
+//
+//Main Page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+// 
 
+// Create New URL page render
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+//
+
+// Delete URL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect(`/urls`);
+});
+//
+
+
+
 
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
@@ -49,20 +61,45 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL; 
+  if (!req.body.longURL) {
+    longURL = urlDatabase[shortURL];
+    const templateVars = { shortURL: shortURL, longURL: longURL };
+    res.render("urls_show", templateVars);
+  } else {
+    longURL = req.body.longURL
+    urlDatabase[shortURL] = longURL;
+    res.redirect(`/urls`);
+  }
+})
+
+
+// Redirect short URL to long URL site 
 app.get("/u/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   let longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
 
+// KEEP 
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
+// don't think I need
+
+// app.post("/urls/:id", (req, res) => {
+//   let newLongURL = req.body.longURL
+//   console.log(newLongURL)
+//   urlDatabase[id] = newLongURL;
+//   res.render("urls_index");
+// })
